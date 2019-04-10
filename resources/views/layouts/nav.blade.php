ce7847527c4d22be84d8f304996c949c6032ec94
Alan Torquato <alantorquato58@gmail.com>
2019-04-10 15:09:31 -0300

    search dos posts

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,35 +1,56 @@
-  <header>
+<header>
     <div class="navbar-fixed">
-      <nav>
-        <div class="nav-wrapper">
-          <a href="#" class="brand-logo"><img class="imagem"src="images/safe.png" alt="Logotipo da Safe Import"></a>
-          <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+        <nav>
+            <div class="nav-wrapper">
+                <a href="#" class="brand-logo"><img class="imagem" src="images/safe.png" alt="Logotipo da Safe Import"></a>
+                <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
 
-          <ul class="right hide-on-med-and-down">
-            <li id="menu-home"><a>Home</a></li>
-            <li id="menu-sobre"><a>Sobre nós</a></li>
-            <li id="menu-como-funciona"><a>Como funciona</a></li>
-            <li id="menu-duvidas"><a>Dúvidas frequentes</a></li>
+                <ul class="right hide-on-med-and-down">
+                    <li>
+                        <form action="/posts/search" method="POST">
+                            {{ csrf_field() }}
+                            <div class="input-field">
+                                <input id="search" type="search" name="criterio" >
+                                <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+                                <i class="material-icons">close</i>
+                            </div>
+                        </form>
+                    </li>
+                    <li id="menu-home"><a>Home</a></li>
+                    <li id="menu-sobre"><a>Sobre nós</a></li>
+                    <li id="menu-como-funciona"><a>Como funciona</a></li>
+                    <li id="menu-duvidas"><a>Dúvidas frequentes</a></li>
+                    <li><a>Notícias</a></li>
+                    <li id="menu-contato"><a>Contato</a></li>
+                    @if (Auth::check())
+                        <li><label>Usuário logado: {{ Auth::user()->name }}</label></li>
+                        <li><a href="/logout" title="Sair"><i class="material-icons sair">exit_to_app</i></a></li>
+                    @endif
+                </ul>
+            </div>
+        </nav>
+
+        <ul class="sidenav" id="mobile-demo">
+            <li id="menu-mobile-home"><a>Home</a></li>
+            <li id="menu-mobile-sobre"><a>Sobre nós</a></li>
+            <li id="menu-mobile-como-funciona"><a>Como funciona</a></li>
+            <li id="menu-mobile-duvidas"><a>Dúvidas frequentes</a></li>
             <li><a>Notícias</a></li>
-            <li id="menu-contato"><a>Contato</a></li>
+            <li id="menu-mobile-contato"><a>Contato</a></li>
+            <li>
+                <form action="/posts/search" method="POST">
+                    {{ csrf_field() }}
+                    <div class="input-field">
+                        <input id="search" type="search" name="criterio" >
+                        <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+                        <i class="material-icons">close</i>
+                    </div>
+                </form>
+            </li>
             @if (Auth::check())
-            <li><label>Usuário logado: {{ Auth::user()->name }}</label></li>
-            <li><a href="/logout" title="Sair"><i class="material-icons sair">exit_to_app</i></a></li>
+                <li id="menu-mobile-sair"><a href="/logout" title="Sair"><i class="material-icons sair">exit_to_app</i>Sair</a>
+                </li>
             @endif
-          </ul>
-        </div>
-      </nav>
-
-      <ul class="sidenav" id="mobile-demo">
-        <li id="menu-mobile-home"><a>Home</a></li>
-        <li id="menu-mobile-sobre"><a>Sobre nós</a></li>
-        <li id="menu-mobile-como-funciona"><a>Como funciona</a></li>
-        <li id="menu-mobile-duvidas"><a>Dúvidas frequentes</a></li>
-        <li><a>Notícias</a></li>
-        <li id="menu-mobile-contato"><a>Contato</a></li>
-        @if (Auth::check())
-        <li id="menu-mobile-sair" ><a href="/logout" title="Sair"><i class="material-icons sair">exit_to_app</i>Sair</a></li>
-        @endif
-      </ul>
+        </ul>
     </div>
-  </header>
+</header>
