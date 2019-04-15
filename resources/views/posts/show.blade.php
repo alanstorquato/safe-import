066a87d68e24106e6447fb6e16c90114ed022f11
Leonardo Bergamini <leonardo.bergamini@hotmail.com.br>
2019-04-15 19:47:00 -0300

    Correção da imagem da página de exibição de notícia.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -84,27 +84,25 @@
     <main>
         <div class="container">
             <div class="row">
-                <div class="row">
-                    <h2 class="center">{{ $post->title }}</h2>
-                    <h5 class="center">{{ $post->subtitle}}</h5>
-                    <img class="responsive-img center" src="{{ asset($post->image) }}" alt="">    
-                </div>
-                <div class="row">
-                    <div class="texto-noticia">
-                        <p>{!! $post->body !!} </p>
-                    </div>
-                </div>
-                <div class="row">
-                    <h6 class="right autor">- {{ $post->user->name }}, {{ $post->created_at->toFormattedDateString() }}</h6>
-                </div>
-                <br>
-                <div class="row">
-                    <div class="container center">
-                        <a href="/" class="waves-effect waves-light btn-large green btn-custom">Voltar</a>
-                    </div>
-                </div>
-                
+                <h2 class="center">{{ $post->title }}</h2>
+                <h5 class="center">{{ $post->subtitle}}</h5>
+                <img class="center img-noticia" src="{{ asset($post->image) }}" alt="Imagem do post">    
             </div>
+            <div class="row">
+                <div class="texto-noticia">
+                    <p class="flow-text">{!! $post->body !!} </p>
+                </div>
+            </div>
+            <div class="row">
+                <h6 class="right autor">- {{ $post->user->name }}, {{ $post->created_at->toFormattedDateString() }}</h6>
+            </div>
+            <br>
+            <div class="row">
+                <div class="container center">
+                    <a href="/" class="waves-effect waves-light btn-large green btn-custom">Voltar</a>
+                </div>
+            </div>
+            
         </div>
     </main>
 
